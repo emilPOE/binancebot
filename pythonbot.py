d14b3437e9c1505def251c65a7cde3b8a3a627a7
Emil Pöhler <poehleremil@gmail.com>
2023-04-08 22:14:42 +0200

    noopenorders

--- pythonbot.py
+++ pythonbot.py
@@ -30,12 +30,12 @@ while True:
                 if order['side'] == 'SELL' and order['type'] == 'LIMIT':
                     open_order_value += decimal.Decimal(order['price']) * decimal.Decimal(order['origQty'])
 
-        total_balance = initial_balance + (open_order_value - 83)
+        total_balance = initial_balance + (open_order_value)
         # Lookup-Tabelle für tusd_amounts basierend auf num_open_orders
         tusd_amount_lookup = {
             0: 0.1,
             1: 0.1,
-            2: 0.2,
+            2: 0.1,
             3: 0.2,
             4: 0.2,
             5: 0.3,
@@ -48,7 +48,7 @@ while True:
             tusd_amount = total_balance
         tusd_amount = tusd_amount // 1
 
-        print(f"Current TUSD Balance: {tusd_balance['free']}. Total Balance (including {num_open_orders} open limit sell orders): {(total_balance + 83):.4f}")
+        print(f"Current TUSD Balance: {tusd_balance['free']}. Total Balance (including {num_open_orders} open limit sell orders): {(total_balance):.4f}")
 
         if tusd_balance is not None and decimal.Decimal(tusd_balance["free"]) >= decimal.Decimal(tusd_amount):
 
@@ -59,7 +59,7 @@ while True:
             avg_price = decimal.Decimal(order["fills"][0]["price"])
 
             # Calculate the sell price with 0.1% profit
-            profit = 0.0005 * (1 + (num_open_orders - 2))
+            profit = 0.0005 * (1 + num_open_orders)
             profit_percent = decimal.Decimal(profit)
             sell_price = avg_price * (decimal.Decimal(1) + profit_percent)
 
@@ -106,7 +106,7 @@ while True:
 
     # Wait for 60 seconds before executing the next iteration of the loop
     num_open_orders = len(client.get_open_orders(symbol=symbol))
-    wait_time = (60 * 5) * (2 ** (num_open_orders - 2))
+    wait_time = (60 * 5) * (2 ** (1 + num_open_orders))
     minutes, seconds = divmod(wait_time, 60)
     print(f"wait for: {minutes:02d}min {seconds:02d}sec")
     print("-----------------------------------------------------------------")
